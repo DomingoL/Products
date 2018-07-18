40f22caaa4f50b4d1c297c1f54323193a8f91c25
DomingoL <dlezcano@imaginesoft.com.py>
2018-07-17 22:21:05 -0400

    Una clase nueva para Ubications Se agregó controlador en el backend y en la API

--- Products.Backend/Models/DataContextLocal.cs
+++ Products.Backend/Models/DataContextLocal.cs
@@ -10,5 +10,7 @@ namespace Products.Backend.Models
     public class DataContextLocal : DataContext
     {
         public System.Data.Entity.DbSet<Products.Domain.Customer> Customers { get; set; }
+
+        public System.Data.Entity.DbSet<Products.Domain.Ubication> Ubications { get; set; }
     }
 }
