b9ba4e2b56456fe0629c4e07aed652e627abefea
Domingo Lezcano <mingo_lezcano@hotmail.com>
2018-07-21 18:06:57 -0400

    asff

---
+++ Products/Products/View/LoginFacebookView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Xamarin.Forms;
+
+namespace Products.View
+{
+    using System;
+    using System.Collections.Generic;
+
+    public partial class LoginFacebookView : ContentPage
+    {
+        public LoginFacebookView()
+        {
+             InitializeComponent();
+        }
+    }
+}
